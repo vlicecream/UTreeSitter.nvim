dc0e18121e2115f7bf68c4b6f15ead824c1c6b6e
vlicecream <vlicecream@163.com>
2026-05-14 11:17:28 +0800

    Fix Unreal macro line indentation

--- queries/unreal_cpp/indents.scm
+++ queries/unreal_cpp/indents.scm
@@ -6,14 +6,14 @@
 (unreal_reflected_struct_declaration) @indent.begin
 (unreal_reflected_enum_declaration) @indent.begin
 
-; UPROPERTY / UFUNCTION blocks often sit inside class bodies.
-; UPROPERTY / UFUNCTION 常出现在类体内，保持正常缩进即可。
-(unreal_property_macro) @indent.begin
-(unreal_function_macro) @indent.begin
+; UPROPERTY / UFUNCTION are line-prefix macros, not indentation blocks.
+; Pressing <CR> after `UPROPERTY()` should keep the next line aligned with the
+; macro line; only pressing <CR> inside the parentheses should indent via the
+; normal `(` rule.
+; UPROPERTY / UFUNCTION 是行前缀宏，不应在宏后回车时额外进一层。
 
 ; Field initializer lists and control flow should behave like stock C++.
 ; 字段初始化列表和控制流保持和原生 C++ 一致。
 (field_initializer_list) @indent.begin
 (condition_clause) @indent.begin
 (access_specifier) @indent.branch
-
